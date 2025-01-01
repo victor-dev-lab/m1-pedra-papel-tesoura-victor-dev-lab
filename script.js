576e396552a9ee1a3a19148be383e6b66891b6e2
Victor Daniel <victor.tec.dev@gmail.com>
2025-01-01 19:53:35 -0300

    Implementação do algoritmo da função playRockPaperScissor() em Javascript.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    if (player1 == "Pedra") {
+        if (player2 == "Pedra") {
+            return "Empate!"
+        }
+        else if (player2 == "Papel") {
+            return "Jogador 2 venceu!"
+        } else {
+            return "Jogador 1 venceu!"
+        }
+    } else if (player1 == "Papel") {
+        if (player2 == "Pedra") {
+            return "Jogador 1 venceu!"
+        } else if (player2 == "Papel") {
+            return "Empate!"
+        } else {
+            return "Jogador 2 venceu!"
+        }
+    } else {
+        if (player2 == "Pedra") {
+            return "Jogador 2 venceu!"
+        } else if (player2 == "Papel") {
+            return "Jogador 1 venceu!" 
+        } else {
+            return "Empate!"
+        }
+    }
+}
+
+const resultado = playRockPaperScissor("Pedra", "Tesoura");
+console.log(resultado);
